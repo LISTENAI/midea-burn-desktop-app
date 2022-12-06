59649643744b6931770b79b01593396764f19f58
jameswang <105700413+listenai-jw@users.noreply.github.com>
2022-12-06 18:19:17 +0800

    (add) error code for mes form

--- ListenAI.Factory.FirmwareDeploy/MesSettingsForm.cs
+++ ListenAI.Factory.FirmwareDeploy/MesSettingsForm.cs
@@ -54,6 +54,8 @@ namespace ListenAI.Factory.FirmwareDeploy {
         }
 
         private void EnableAllControls(bool isEnabled) {
+            btnDbImport.Text = isEnabled ? "导入" : "Loading...";
+
             tbMesCmdId.Enabled = isEnabled;
             tbProdId.Enabled = isEnabled;
             tbProdName.Enabled = isEnabled;
@@ -85,7 +87,7 @@ namespace ListenAI.Factory.FirmwareDeploy {
             }
 
             if (tbMesCmdId.Text == "") {
-                MessageBox.Show("请输入MES指令单号", "错误");
+                MessageBox.Show("[201-1] 请输入MES指令单号", "错误");
                 return;
             }
 
@@ -94,13 +96,13 @@ namespace ListenAI.Factory.FirmwareDeploy {
                 string.IsNullOrWhiteSpace(tbDbUsername.Text) ||
                 string.IsNullOrWhiteSpace(tbDbPassword.Text) ||
                 string.IsNullOrWhiteSpace(tbDbTableName.Text)) {
-                MessageBox.Show("数据库连接信息不完整，请补充。", "错误");
+                MessageBox.Show("[201-2] 数据库连接信息不完整，请补充。", "错误");
                 return;
             }
 
             var tableNameTest = Regex.IsMatch(tbDbTableName.Text, @"^[A-Za-z0-9_-]*$", RegexOptions.Singleline);
             if (!tableNameTest) {
-                MessageBox.Show("数据库表名不正确", "错误");
+                MessageBox.Show("[201-3] 数据库表名不正确", "错误");
                 return;
             }
 
@@ -113,36 +115,34 @@ namespace ListenAI.Factory.FirmwareDeploy {
             var mesCmdId = tbMesCmdId.Text;
 
             try {
-                using (var conn = Utils.CreateMsSqlConnection(server, username, password, db)) {
-                    var sql = "select top 1 DAA001, DAA014, DAA015, DAA016 from " + tableName + " where DAA001 = @mesCmdId";
-                    var cmd = new SqlCommand(sql, conn);
-                    cmd.Parameters.AddWithValue("@mesCmdId", mesCmdId);
+                using var conn = Utils.CreateMsSqlConnection(server, username, password, db);
+                var sql = "select top 1 DAA001, DAA014, DAA015, DAA016 from " + tableName + " where DAA001 = @mesCmdId";
+                var cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@mesCmdId", mesCmdId);
 
-                    using (var reader = cmd.ExecuteReader()) {
-                        if (!reader.HasRows) {
-                            EnableAllControls(true);
-                            MessageBox.Show("根据MES指令单号，没有找到任何记录。01", "错误");
-                            return;
-                        }
+                using var reader = cmd.ExecuteReader();
+                if (!reader.HasRows) {
+                    throw new ListenAiException(202, "根据MES指令单号，没有找到任何记录。", "", 1);
+                }
 
-                        if (reader.Read()) {
-                            tbProdId.Text = reader["DAA014"].ToString();
-                            tbProdName.Text = reader["DAA015"].ToString();
-                            tbProdModel.Text = reader["DAA016"].ToString();
-                        }
-                        else {
-                            EnableAllControls(true);
-                            MessageBox.Show("根据MES指令单号，没有找到任何记录。02", "错误");
-                            return;
-                        }
-                    }
+                if (reader.Read()) {
+                    tbProdId.Text = reader["DAA014"].ToString();
+                    tbProdName.Text = reader["DAA015"].ToString();
+                    tbProdModel.Text = reader["DAA016"].ToString();
+                }
+                else {
+                    throw new ListenAiException(202, "根据MES指令单号，没有找到任何记录。", "", 2);
                 }
             }
+            catch (ListenAiException lex) {
+                var exCode = lex.SubCode != 0 ? $"{lex.Code:000}-{lex.SubCode}" : lex.Code.ToString();
+                MessageBox.Show($"[{exCode}] {lex.Message}\n{lex.Details}", "错误");
+            }
             catch (SqlException sqlEx) {
-                MessageBox.Show($"无法连接数据库，请联系运维处理。\n{sqlEx.Message}", "错误");
+                MessageBox.Show($"[203] 无法连接数据库，请联系运维处理。\n{sqlEx.Message}", "错误");
             }
             catch (Exception ex) {
-                MessageBox.Show($"连接数据库出现异常，请联系运维处理。\n{ex.Message}", "错误");
+                MessageBox.Show($"[204] 连接数据库出现异常，请联系运维处理。\n{ex.Message}", "错误");
             }
 
             EnableAllControls(true);
@@ -156,7 +156,7 @@ namespace ListenAI.Factory.FirmwareDeploy {
                 string.IsNullOrWhiteSpace(tbFlashOpter.Text) ||
                 string.IsNullOrWhiteSpace(tbFlashToolName.Text) ||
                 string.IsNullOrWhiteSpace(tbFlashMachineId.Text)) {
-                MessageBox.Show("请完全填写MES记录需要的数据后再点击确认", "错误");
+                MessageBox.Show("[201-4] 请完全填写MES记录需要的数据后再点击确认", "错误");
                 return;
             }
 
