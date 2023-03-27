4bfc02dd3b313d3de05ff326c01f20e6b184fca1
jameswang <105700413+listenai-jw@users.noreply.github.com>
2023-03-27 10:37:22 +0800

    version -> 2.0.0

--- ListenAI.Factory.FirmwareDeploy/MainForm.Designer.cs
+++ ListenAI.Factory.FirmwareDeploy/MainForm.Designer.cs
@@ -573,7 +573,7 @@
             MaximizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "聆思模组烧录工具 v2.0.0-beta.3";
+            Text = "聆思模组烧录工具 v2.0.0";
             FormClosing += MainForm_FormClosing;
             FormClosed += MainForm_FormClosed;
             Load += MainForm_Load;
