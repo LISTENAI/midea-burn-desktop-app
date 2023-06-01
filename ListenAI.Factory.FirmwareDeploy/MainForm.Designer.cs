adc519c9b6818bcf4c61b81bc4a9ad3376f4976b
jw <105700413+listenai-jw@users.noreply.github.com>
2023-06-01 13:11:40 +0800

    set restiction for stopping instance from running in same path

--- ListenAI.Factory.FirmwareDeploy/MainForm.Designer.cs
+++ ListenAI.Factory.FirmwareDeploy/MainForm.Designer.cs
@@ -572,7 +572,7 @@
             MaximizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "聆思模组烧录工具 v2.1.0-edu";
+            Text = "聆思模组烧录工具 v2.1.0-beta.2-edu";
             FormClosing += MainForm_FormClosing;
             FormClosed += MainForm_FormClosed;
             Load += MainForm_Load;
